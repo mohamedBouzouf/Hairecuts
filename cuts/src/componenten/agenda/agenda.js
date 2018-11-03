e1f99bd79bf505bf7024b644fe8561cc4f972bdf
Berton <berton.lutina@veb.be>
2018-11-03 17:57:50 +0100

    add Files and Components

---
+++ cuts/src/componenten/agenda/agenda.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Agenda extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Agenda
