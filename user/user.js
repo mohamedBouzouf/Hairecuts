0a89af9ad6e2c638084491c54c06e0c31fd61bff
mohamedBouzouf <mohamed.bouzouf@student.odisee.be>
2018-11-22 13:06:30 +0100

    polymer

---
+++ user/user.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import 'semantic-ui-css/semantic.min.css';
+class User extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default User
